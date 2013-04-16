7640277c0830372432f0b0f38b0cc7a20903088a
zhaoxu <zhaoxu.email@gmail.com>
2013-04-17 01:35:41 +0800

    Change client on receiving message logic

--- src/com/exhibition/netty/client/ClientHandler.java
+++ src/com/exhibition/netty/client/ClientHandler.java
@@ -1,119 +1,113 @@
 package com.exhibition.netty.client;
 
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.jboss.netty.channel.ChannelHandlerContext;
-import org.jboss.netty.channel.ChannelStateEvent;
-import org.jboss.netty.channel.ExceptionEvent;
-import org.jboss.netty.channel.MessageEvent;
-import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
-
-import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender.SendIntentException;
 import android.util.Log;
-
-import com.exhibition.HomeActivity;
 import com.exhibition.MessageActivity;
 import com.exhibition.conts.StringPools;
 import com.exhibition.db.XmlDB;
 import com.exhibition.reciver.MessageReciver;
-import com.exhibition.service.SocketService;
-import com.exhibition.utils.NotificationUtil;
 import com.exhibition.utils.Resources;
+import org.jboss.netty.channel.*;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 监听消息回调
- * @author pjq
  *
+ * @author pjq
  */
 public class ClientHandler extends SimpleChannelUpstreamHandler {
-	private Context context;
-	private Intent intent = new Intent();
-	private boolean isCheckin = false;
-	private String recivedMessage; 
-	
-	public ClientHandler(Context context) {
-		this.context = context;
-	}
-	public ClientHandler() {
-		
-	}
-	
-	/**
-	 * linkService(socket连接)回调
-	 */
-	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e)
-			throws Exception {
-		
-		recivedMessage = e.getMessage().toString();
-		if (!e.getMessage().equals(null) && !e.getMessage().equals("")) {
-			try {
-				XmlDB.getInstance(context).saveKey(StringPools.mServiceToken,recivedMessage);
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}  
-		}
-		e.getChannel().write("byte");
-		if(isCheckin ){
-			addMessageToList();
-			Notification notification = new Notification(android.R.id.icon, 
-														 recivedMessage,
-														 System.currentTimeMillis());
-			if(!recivedMessage.equals(":[PING:PONG]")){
-				intent = new Intent(context,MessageActivity.class);
-				PendingIntent pendingIntent = PendingIntent.getActivity(
-								context,0, intent, 0);
-				
-				notification.setLatestEventInfo(context, "推送的消息", recivedMessage, pendingIntent);	//通知列表里的显示情况
-				NotificationManager noManager = (NotificationManager) context.getSystemService(
-									Context.NOTIFICATION_SERVICE);
-				notification.defaults = Notification.DEFAULT_SOUND;
-				noManager.notify(110, notification);
-			}
-		}else{
-			intent = new Intent(context,MessageReciver.class);
-			intent.putExtra("latitude", Resources.latitude);
-			intent.putExtra("longitude", Resources.longitude);
-			intent.putExtra("address", Resources.address);
-			context.sendBroadcast(intent);
-			isCheckin = true;
-		}
-	}
+    public static final String HEART_BEAT = ":[PING:PONG]";
+    private Context context;
+    private boolean checkedIn;
 
-	
-	private void addMessageToList() {
-		if(!recivedMessage.equals(":[PING:PONG]")){
-			Map<String,Object> map = new HashMap<String, Object>();
-			Calendar calendar  = Calendar.getInstance();
-			int hour = calendar.get(Calendar.HOUR_OF_DAY);
-			int minute = calendar.get(Calendar.MINUTE);
-			StringBuilder time = new StringBuilder();
-			time.append(hour + ":");
-			time.append(minute + "  ");
-			map.put("timeAndContent", time.toString() + "         " + recivedMessage);
-			Resources.messageMap.add(map);
-		}
-	}
-	
-	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e)
-			throws Exception {
-		e.getChannel().close();
-	}   
-	
-	@Override
-	public void channelDisconnected(ChannelHandlerContext ctx,
-			ChannelStateEvent e) throws Exception {
-		super.channelDisconnected(ctx, e);
-Log.i("data","reconnect");
-		/*Intent intent = new Intent(context,SocketService.class);
+    public ClientHandler(Context context) {
+        this.context = context;
+    }
+
+    public ClientHandler() {
+    }
+
+    /**
+     * linkService(socket连接)回调
+     */
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+        String message = e.getMessage().toString();
+        if (message == null) {
+            return;
+        }
+        while (message.startsWith(HEART_BEAT)) {
+            message = message.substring(HEART_BEAT.length());
+        }
+        if (message.isEmpty()) {
+            return;
+        }
+        // It is not be able to divide service token from text message for now.
+        // Just assuming the first message received will be service token, but eventually,
+        // the doom day will be there.
+        if (!checkedIn) {
+            try {
+                checkedIn = true;
+                XmlDB.getInstance(context).saveKey(StringPools.mServiceToken, message);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            addMessageToList(message);
+            Notification notification = new Notification(android.R.id.icon, message, System.currentTimeMillis());
+            Intent intent = new Intent(context, MessageActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            notification.setLatestEventInfo(context, "推送的消息", message, pendingIntent);
+            NotificationManager noManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notification.defaults = Notification.DEFAULT_SOUND;
+            noManager.notify(110, notification);
+        }
+    }
+
+    @Override
+    public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        super.channelConnected(ctx, e);
+        Intent intent = new Intent(context, MessageReciver.class);
+        intent.putExtra("latitude", Resources.latitude);
+        intent.putExtra("longitude", Resources.longitude);
+        intent.putExtra("address", Resources.address);
+        context.sendBroadcast(intent);
+    }
+
+    private void addMessageToList(String message) {
+        if (!message.equals(":[PING:PONG]")) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            Calendar calendar = Calendar.getInstance();
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
+            StringBuilder time = new StringBuilder();
+            time.append(hour + ":");
+            time.append(minute + "  ");
+            map.put("timeAndContent", time.toString() + "         " + message);
+            Resources.messageMap.add(map);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+        try {
+            e.getChannel().close();
+        } catch (Exception ignore) {
+        }
+    }
+
+    @Override
+    public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        super.channelDisconnected(ctx, e);
+        Log.i("data", "reconnect");
+        /*Intent intent = new Intent(context,SocketService.class);
 		context.startService(intent);*/
 
-	}
+    }
 }
--- src/com/exhibition/netty/client/MyClient.java
+++ src/com/exhibition/netty/client/MyClient.java
@@ -1,96 +1,92 @@
 package com.exhibition.netty.client;
 
-import java.net.InetSocketAddress; 
-import java.nio.charset.Charset;
-import java.util.concurrent.Executors;
-
+import android.content.Context;
 import org.jboss.netty.bootstrap.ClientBootstrap;
-import org.jboss.netty.channel.ChannelFactory;
-import org.jboss.netty.channel.ChannelFuture;
-import org.jboss.netty.channel.ChannelPipeline;
-import org.jboss.netty.channel.ChannelPipelineFactory;
-import org.jboss.netty.channel.DefaultChannelPipeline;
-import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.*;
 import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
 import org.jboss.netty.handler.codec.string.StringDecoder;
 import org.jboss.netty.handler.codec.string.StringEncoder;
 
-import android.content.Context;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.util.concurrent.Executors;
 
 
 public class MyClient {
-	
-	private ClientBootstrap bootstrap;
-	//private Context context;
-	public interface MessageListener {
-        public void onMessageReceived(MessageEvent e);
-    }
-	public MyClient() {
-		bootstrap = new ClientBootstrap((ChannelFactory) new NioClientSocketChannelFactory(
+
+    private ClientBootstrap bootstrap;
+
+    public MyClient() {
+        bootstrap = new ClientBootstrap((ChannelFactory) new NioClientSocketChannelFactory(
                 Executors.newCachedThreadPool(), Executors
-                .newCachedThreadPool()));   
+                .newCachedThreadPool()));
         bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
             @Override
             public ChannelPipeline getPipeline() throws Exception {
                 ChannelPipeline result = new DefaultChannelPipeline();
-                
+
                 result.addLast("encode", new StringEncoder(Charset.forName("UTF-8")));
                 result.addLast("decode", new StringDecoder(Charset.forName("UTF-8")));
                 result.addLast("handler", new ClientHandler());
                 return result;
             }
         });
-        
-        bootstrap.setOption("tcpNoDelay", true); //设置选项集
+
+        bootstrap.setOption("tcpNoDelay", true);
         bootstrap.setOption("keepAlive", true);
-	}
-	public MyClient(final Context context) {
-		bootstrap = new ClientBootstrap((ChannelFactory) new NioClientSocketChannelFactory(
+    }
+
+    public MyClient(final Context context) {
+        bootstrap = new ClientBootstrap((ChannelFactory) new NioClientSocketChannelFactory(
                 Executors.newCachedThreadPool(), Executors
-                .newCachedThreadPool()));   
+                .newCachedThreadPool()));
         bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
             @Override
             public ChannelPipeline getPipeline() throws Exception {
                 ChannelPipeline result = new DefaultChannelPipeline();
-                
-                result.addLast("encode", new StringEncoder());
-                result.addLast("decode", new StringDecoder());
+
+                result.addLast("encode", new StringEncoder(Charset.forName("UTF-8")));
+                result.addLast("decode", new StringDecoder(Charset.forName("UTF-8")));
                 result.addLast("handler", new ClientHandler(context));
                 return result;
             }
-        });         
-        bootstrap.setOption("tcpNoDelay", true); //设置选项集
-        bootstrap.setOption("keepAlive", true); 
+        });
+        bootstrap.setOption("tcpNoDelay", true);
+        bootstrap.setOption("keepAlive", true);
     }
-	
-	/**
-	 * 建立socket连接
--	 * @param host 地址
--	 * @param poot 端口号
-	 */ 
-    private ChannelFuture getChannelFuture(final String host,final int poot) {
-    	
-    	try{
-    		ChannelFuture channelFuture  = bootstrap.connect(new InetSocketAddress(host, poot));
-    		channelFuture.awaitUninterruptibly();
-    		if (!channelFuture.isSuccess()) {  
+
+    /**
+     * 建立socket连接
+     * -	 * @param host 地址
+     * -	 * @param poot 端口号
+     */
+    private ChannelFuture getChannelFuture(final String host, final int poot) {
+        try {
+            ChannelFuture channelFuture = bootstrap.connect(new InetSocketAddress(host, poot));
+            channelFuture.awaitUninterruptibly();
+            if (!channelFuture.isSuccess()) {
                 channelFuture.getChannel().getCloseFuture().awaitUninterruptibly();
                 return null;
             }
-    		return channelFuture;
-    	}catch(Exception e){
-    		e.printStackTrace();
-    		return null;
-    	}
-        
+            return channelFuture;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
-    public void send(final String jSonMessage,final String host,final int poot) { 
-    	
-        ChannelFuture future = getChannelFuture(host,poot);		
+    public void send(final String jSonMessage, final String host, final int poot) {
+
+        ChannelFuture future = getChannelFuture(host, poot);
         if (future != null) {
             future.getChannel().write(jSonMessage);
         }
     }
-    
+
+    //private Context context;
+    public interface MessageListener {
+        public void onMessageReceived(MessageEvent e);
+    }
+
 }
